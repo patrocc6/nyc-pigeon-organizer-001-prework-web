efa6628e9719fba386001a4063fbded5ce0d3ed4
Pat Rocchio <patrocc6@gmail.com>
2016-02-20 14:03:50 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+	pigeon_list = Hash.new { |hash, key| hash[key] = Hash.new { |hash, key| hash[key] = [] } }
+	data.each do |attribute, attr_choice|
+		attr_choice.each do |attr_data, pigeons|
+			pigeons.each do |pigeon|
+				pigeon_list[pigeon][attribute] << attr_data.to_s
+			end
+		end
+	end
+	pigeon_list
 end
